c719e24deecb7190ba30ec2c8542e2e1dd86d540
magmueller <mmagnus@ethz.ch>
2024-04-16 13:54:38 +0200

    Works on gpu cluster

--- euler_playground_commands/interactive.sh
+++ euler_playground_commands/interactive.sh
@@ -3,12 +3,22 @@
 #singularity exec --nv modulus_23.08.sif python simple_inference.py
 #apptainer shell --nv --bind /cluster:/cluster $SCRATCH/modulus_23.08.sif
 cd $SCRATCH/earth2mip/fcnv2_sm/
-module load cuda/11.8.0
-module load gcc/6.3.0
-module spider  python_gpu/3.7.4
-module load gcc/8.2.0 python/3.10.4
-singularity exec --nv --bind /cluster:/cluster $SCRATCH/modulus_23.08.sif python simple_inference.py
+#module spider  python_gpu/3.7.4
 
+module load cuda/12.1.1
+module load gcc/8.2.0 python/3.10.4
+
+export LD_LIBRARY_PATH=/cluster/apps/nss/gcc-8.2.0/python/3.10.4/lib:$LD_LIBRARY_PATH
+singularity exec --nv --bind /cluster:/cluster $SCRATCH/modulus_23.08.sif python simple_inference.py
+singularity run --nv --compat --bind /cluster:/cluster $SCRATCH/modulus_23.08.sif
+
+
+
+srun --gpus=rtx_3090:1 --mem-per-cpu=8G --time=02:00:00 --pty bash -i 
+
+singularity run --nv --compat --pwd $SCRATCH/models --bind /cluster:/cluster $SCRATCH/modulus_23.08.sif
+python fcnv2_sm/simple_inference.py
+nvcc --version
 #  srun --gpus=rtx_3090:1 --mem-per-cpu=8G --time=02:00:00 --pty bash -i
 # source venv/bin/activate
 #python simple_inference.py
