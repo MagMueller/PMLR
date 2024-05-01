be5d12c76f4a27f48954f02151d1fa598cba2bce
magmueller <mamagnus00@gmail.com>
2024-05-01 23:05:05 +0200

    Train loop for sequence data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # %% Imports
 import matplotlib.pyplot as plt
+from scipy import sparse
 import torch_geometric
 import numpy as np
 from torch_geometric.data import Data
@@ -20,10 +21,10 @@ import os
 OUTPUT_PATH = "output"
 DATA_PATH = "./ccai_demo/data/FCN_ERA5_data_v0/out_of_sample"
 DATA_FILE = os.path.join(DATA_PATH, "2018.h5")
-BATCH_SIZE = 64
+BATCH_SIZE = 1
 EPOCHS = 100
 LEARNING_RATE = 0.001
-
+SEQUENCE_LENGTH = 3
 N_LAYER = 3
 N_HIDDEN = 64
 DT = 1.
@@ -50,66 +51,74 @@ VARIABLES = ['u10',
              'r500',
              'r850',
              'tcwv']
-N_VAR = len(VARIABLES)
+N_VAR = 21  # somehow the len here is just 20
+print(f"Number of variables: {N_VAR}")
+if torch.backends.mps.is_available():
+    DEVICE = "mps"
+    # set env var PYTORCH_ENABLE_MPS_FALLBACK=1
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
-DEVICE = "mps" if torch.backends.mps.is_available(
-) else "cuda" if torch.cuda.is_available() else "cpu"
+elif torch.cuda.is_available():
+    DEVICE = "cuda"
+else:
+    DEVICE = "cpu"
+
 
 # %% - Load data
 
 
 class H5GeometricDataset(torch.utils.data.Dataset):
-    def __init__(self, path):
+    # data size is [batch, n_var, x, y] x = 721, y = 1440 for ERA5 n_var = 21
+    # we want a Graph with 721*1440 nodes and 21 features and each node is connected to its 8 neighbors
+    def __init__(self, path, sequence_length=3):
         self.file_path = path
         self.dataset = None
         with h5py.File(self.file_path, 'r') as file:
             # print keys
-            print("Keys: %s" % file.keys())
             self.dataset_len = len(file["fields"])
-            print(f"Dataset length: {self.dataset_len}")
         self.height = 721
         self.width = 1440
         self.features = 21
+        # approx 800 Mio values -> 3.2 GB
+        self.sequence_length = sequence_length
         self.edge_index = self.create_edge_index(self.height, self.width)
-        self.adj = SparseTensor(row=self.edge_index[0], col=self.edge_index[1],
-                                sparse_sizes=(self.height*self.width, self.height*self.width))
+        # TODO to sparse
 
     def __getitem__(self, index):
         if self.dataset is None:
             self.dataset = h5py.File(self.file_path, 'r')["fields"]
-        data_array = self.dataset[index]
-        data_array = data_array.reshape(-1, self.features)
-        x = torch.tensor(data_array, dtype=torch.float)
-
-        return x, self.adj
+        # load a sequence of 5 time steps
+        sequences = [self.dataset[i].reshape(-1, self.features)
+                     for i in range(index, index + self.sequence_length + 1)]
+        # numpy array
+        sequences = np.stack(sequences, axis=0)
+        # to torch tensor
+        x = torch.tensor(sequences, dtype=torch.float32).to(DEVICE)
+        return x, self.edge_index
 
     def __len__(self):
-        return self.dataset_len
+        return self.dataset_len - self.sequence_length
 
     def create_edge_index(self, height, width):
-        edge_index = grid(height=height, width=width)
-        edge_index = torch_geometric.utils.to_undirected(edge_index)
+        # 721*1440*9 = 9344160 -> 9331198 edges (not at the border)
+        (row, col), pos = grid(height=height, width=width)
+        edge_index = torch.stack([row, col], dim=0).to(torch.long).to(DEVICE)
+        edge_index = to_undirected(edge_index)
+        # edge_index = edge_index.to_sparse()
+        # edge_index = edge_index.coalesce()
         return edge_index
 
 
-dataset = H5GeometricDataset(DATA_FILE)
+dataset = H5GeometricDataset(DATA_FILE, sequence_length=SEQUENCE_LENGTH)
 train_loader = torch.utils.data.DataLoader(
     dataset, batch_size=BATCH_SIZE, shuffle=True)
-# get sample to load the h5file
-sample = dataset[0]
-print(f"Sample shape: {sample.shape}")
-
-# %% - plot sample
-# unsqueeze batch dimension of numpy array
-sample = torch.tensor(sample).unsqueeze(0)
-timestep_idx = 0
-fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(15, 10))
-for i, varname in enumerate(['u10', 't2m', 'z500', 'tcwv']):
-    cm = 'bwr' if varname == 'u10' or varname == 'z500' else 'viridis'
-    varidx = VARIABLES.index(varname)
-    ax[i//2][i % 2].imshow(sample[timestep_idx, varidx], cmap=cm)
-    ax[i//2][i % 2].set_title(varname)
-fig.tight_layout()
+# %% # get sample
+x, adj = next(iter(train_loader))
+print(f"x shape: {x.shape}, adj shape: {adj.shape}")
+a = x.element_size() * x.nelement() / 1024**2
+print(F"Memory size of x: {a} MB")
+a = adj.element_size() * adj.nelement() / 1024**2
+print(F"Memory size of adj: {a} MB")
 
 # %% Evaluation
 # define metrics from the definitions above
@@ -154,24 +163,48 @@ model = deep_GNN(nfeat=N_VAR, nhid=N_HIDDEN, nclass=N_VAR,
 model.to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-# %%
-len(train_loader.dataset)
-sample.shape
-# %%
-# data size is [batch, n_var, x, y] x = 721, y = 1440 for ERA5 n_var = 21
-# we want a Graph with 721*1440 nodes and 21 features and each node is connected to its 8 neighbors
+
+# %% - Train model
+def train(model, loader, optimizer, criterion, device):
+    model.train()
+    total_loss = 0
+    for batch, data in enumerate(loader):
+        x, edge_index = data
+        # Shape: [batch_size, sequence_length, num_nodes, num_features]
+        x = x.to(device)
+        edge_index = edge_index.to(device).squeeze()
+        print(f"X shape: {x.shape}, edge_index shape: {edge_index.shape}")
+        # Reshape to fit the model's expected input and prepare for rolling prediction
+        batch_size, seq_len, num_nodes, num_features = x.shape
+        losses = []
+
+        optimizer.zero_grad()
+
+        # Iterate over each timestep, predicting the next state
+        for t in range(seq_len - 1):
+            x_input = x[:, t, :, :].view(batch_size, num_nodes, num_features)
+            x_target = x[:, t + 1, :,
+                         :].view(batch_size, num_nodes, num_features)
+
+            # Model output for current timestep
+            predictions = model(x_input, edge_index)
+
+            # Compute loss for the current timestep prediction
+            loss = criterion(predictions, x_target)
+            losses.append(loss)
+            print(f"Loss: {loss.item()} at timestep {t}")
+        # Average loss across the sequence for backpropagation
+        sequence_loss = sum(losses) / len(losses)
+        sequence_loss.backward()
+        optimizer.step()
+        total_loss += sequence_loss.item()
+        print(f"Total loss: {total_loss} at batch {batch}")
+
+    return total_loss / len(loader)
 
 
-# create a grid graph
-# TypeError: grid() missing 1 required positional argument: 'width'
-(row, col), pos = torch_geometric.utils.grid(height=721, width=1440)
-edge_index = torch.stack([row, col], dim=0)
-edge_index = to_undirected(edge_index)
-edge_index = edge_index.to(DEVICE)
-sparse_adj = to_dense_adj(edge_index)
-sparse_adj = dense_to_sparse(sparse_adj)
-# edge indices stay always the same node feature is a time series of 21 features
-# create a graph data object like H5Dataset for
+criteria = nn.MSELoss()
+total_loss = train(model, train_loader, optimizer, criteria, DEVICE)
 
 
 # %%
