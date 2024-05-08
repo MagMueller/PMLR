Normalization fix

utils/dataset.py
@@ -42,7 +42,7 @@ class H5GeometricDataset(torch.utils.data.Dataset):
         self.sequence_length = sequence_length
         self.edge_index = self.create_edge_index(self.height, self.width)
 
-        self.means = np.load(GLOBAL_MEANS_PATH)[0, :features] 
+        self.means = np.load(GLOBAL_MEANS_PATH)[0, :features]
         self.stds = np.load(GLOBAL_STDS_PATH)[0, :features]
         self.means = self.means.reshape(1, 1, features)
         self.stds = self.stds.reshape(1, 1, features)
@@ -54,7 +54,7 @@ class H5GeometricDataset(torch.utils.data.Dataset):
             self.dataset = h5py.File(self.file_path, 'r')["fields"]
         sequences = [self.dataset[i].transpose(1, 2, 0).reshape(-1, self.features)
                      for i in range(index, index + self.sequence_length + 1)]
-        
+
         # numpy array
         sequences = np.stack(sequences, axis=0)
         print(f'shape sequence: {sequences.shape}')
@@ -65,7 +65,7 @@ class H5GeometricDataset(torch.utils.data.Dataset):
         print(f'shape normalized_sequences: {normalized_sequences.shape}')
 
         # to torch tensor
-        x = torch.tensor(sequences, dtype=torch.float32).to(DEVICE)
+        x = torch.tensor(normalized_sequences, dtype=torch.float32).to(DEVICE)
         return x, self.edge_index
 
     def __len__(self):