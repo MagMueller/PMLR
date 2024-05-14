d9274f6277d9370271f205605d1000bdab1f4e72
magmueller <mmagnus@ethz.ch>
2024-05-14 14:45:45 +0200

    Change ressources requested

--- run_exp_parallel.sh
+++ run_exp_parallel.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --nodes=1            # This needs to match Trainer(num_nodes=...)
-#SBATCH --gpus=8
-#SBATCH --ntasks-per-node=8   # This needs to match Trainer(devices=...)
-#SBATCH --time=0-11:59:00
-#SBATCH --mem-per-cpu=8G
+#SBATCH --gpus=2
+#SBATCH --ntasks-per-node=2   # This needs to match Trainer(devices=...)
+#SBATCH --time=0-23:59:00
 #SBATCH --job-name=corrn_job_long   # Job name
-#SBATCH --output=log/corrn_job_long%j.out  # Standard output and error log
-#SBATCH --error=log/corrn_job_long%j.err  # Standard output and error log
-
+#SBATCH --output=log/corrn_job_long_%j.out  # Standard output and error log
+#SBATCH --error=log/corrn_job_long_%j.err  # Standard output and error log
+#SBATCH --mem-per-cpu=8G 
 module load  cuda/11.8.0 
 module load  eth_proxy
 module load gcc/9.3.0 python/3.11.2
@@ -15,4 +14,4 @@ module load gcc/9.3.0 python/3.11.2
 cd $HOME/PMLR
 source pmlr_env/bin/activate
 
-python main.py --nodes 1 --devices 8
+python main.py --nodes 1 --devices 2
